90dde040deb1bc059c629074ceadb29808ac70f2
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-05-26 18:08:33 -0300

    corrected the image dimensions from og:image tag.

--- src/components/PageHead.jsx
+++ src/components/PageHead.jsx
@@ -17,8 +17,8 @@ export default function PageHead() {
             <meta name="copyright" content="Nalbert Cerqueira 2023" />
             <meta property="og:image" content="https://nalbertdev.com/imgs/preview.png" />
             <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="1363" />
-            <meta property="og:image:height" content="653" />
+            <meta property="og:image:width" content="1366" />
+            <meta property="og:image:height" content="652" />
             <meta property="og:locale" content="pt_BR" />
             <meta property="og:url" content="https://nalbertdev.com" />
             <meta property="og:type" content="website" />
