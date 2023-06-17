44a9e0fb06b017ee12f40d1c3eac923d7ccd281b
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-06-17 00:53:40 -0300

    created two new functions to generate  base64Url images to use as placeholders for some <Image/> tags.

--- src/libs/contentful-cms.js
+++ src/libs/contentful-cms.js
@@ -1,4 +1,5 @@
-const contentful = require("contentful")
+import * as contentful from "contentful"
+import { getPlaiceholder } from "plaiceholder"
 
 //Iniciando o client do CMS
 export function initiateClient() {
@@ -11,14 +12,40 @@ export function initiateClient() {
     return client
 }
 
+//Gerando o placeholder da imagem do projeto em base64
+//que será utilizado em blurDataURL
+async function generateBase64Url(url, assetId) {
+    const imgBuffer = await fetch(url).then(async (res) =>
+        Buffer.from(await res.arrayBuffer())
+    )
+    const { base64 } = await getPlaiceholder(imgBuffer)
+    return { base64Url: base64, assetId }
+}
+
+//Gerando um objeto contendo todos os placeholders em base64
+async function generateBase64UrlMap(includes) {
+    const base64Urls = await Promise.all(
+        includes.Asset.map((asset) =>
+            generateBase64Url("https:" + asset.fields.file.url, asset.sys.id)
+        )
+    )
+
+    return base64Urls.reduce((acc, item) => {
+        acc[item.assetId] = item.base64Url
+        return acc
+    }, {})
+}
+
 //Formatando os dados dos projetos vindos do CMS antes de utiliza-los.
-export function organizeProjectsData(entries) {
+export async function organizeProjectsData(entries) {
     const { items, includes } = entries
+    const base64UrlMap = await generateBase64UrlMap(includes)
 
     const bannersData = includes.Asset.reduce((acc, asset) => {
         acc[asset.sys.id] = {
             alt: asset.fields.description,
             url: "https:" + asset.fields.file.url,
+            base64Url: base64UrlMap[asset.sys.id],
             width: asset.fields.file.details.image.width,
             height: asset.fields.file.details.image.height
         }
