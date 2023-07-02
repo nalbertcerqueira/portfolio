5c3d63f9232eeccc45cb34632fc6d675c3cdab2d
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-07-02 12:20:06 -0300

    updates in index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ interface HomeProps {
 
 //Buscando as informações dos projetos e habilidades na contenful CMS.
 export async function getStaticProps(): Promise<GetStaticPropsResult<HomeProps>> {
-    const [projectEntries, skillEntries] = await cms.getProjectsAndSkillsEntries()
+    const [projectEntries, skillEntries] = await cms.fetchData()
 
     const skillsMap = cms.generateSkillsMap(skillEntries)
     const projectList = await cms.getFormattedProjectList(projectEntries)
