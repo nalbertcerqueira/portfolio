8a3e96e3632e8db1e927f224010c745c5a82a735
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-07-02 12:17:36 -0300

    created the cms.d.ts file to wrap all types used in cms service.

---
+++ src/libs/cms.d.ts
@@ -0,0 +1,46 @@
+import { EntryCollection, EntryFieldTypes } from "contentful"
+
+export namespace CMS {
+    export interface ProjectSkeleton {
+        contentTypeId: "portfolioProject"
+        fields: {
+            name: EntryFieldTypes.Text
+            slug: EntryFieldTypes.Integer
+            description: EntryFieldTypes.Text
+            projectUrl: EntryFieldTypes.Text
+            githubUrl: EntryFieldTypes.Text
+            techList: EntryFieldTypes.Array<EntryFieldTypes.Symbol>
+            banner: EntryFieldTypes.AssetLink
+        }
+    }
+
+    export interface SkillSkeleton {
+        contentTypeId: "portfolioSkill"
+        fields: {
+            slug: EntryFieldTypes.Text
+            name: EntryFieldTypes.Text
+            description: EntryFieldTypes.Text
+        }
+    }
+
+    export type CustomModifiers = "WITHOUT_UNRESOLVABLE_LINKS"
+
+    export type ProjectEntryCollection = EntryCollection<ProjectSkeleton, CustomModifiers>
+
+    export type SkillEntryColection = EntryCollection<SkillSkeleton, CustomModifiers>
+
+    export type EntryCollectionList = [ProjectEntryCollection, SkillEntryColection]
+}
+
+export interface Base64Url {
+    id: string
+    url: string
+}
+
+export interface CMSClient {
+    client: Record<string, any>
+
+    public fetchData(): Promise<any[]>
+
+    public generateBase64Image(id: string, url: string): Promise<Base64Url | null>
+}
