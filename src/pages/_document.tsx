7bef789ceb3d37c9dc7be80697c90a80a17107b6
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-07-24 10:16:59 -0300

    removed <link rel='preconnect' href='https://fonts.googleapis.com'> from the document Head because Next automatically provides this header as part of its font optimization.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
         <Html lang="pt-br">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;800&display=swap"
                     rel="stylesheet"
