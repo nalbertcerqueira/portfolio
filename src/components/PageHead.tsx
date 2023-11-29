cb7f6f1dddde6f9a092405892699fef58f331c76
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-29 19:25:17 -0300

    changed mime type for og:image

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -13,7 +13,7 @@ export default function PageHead() {
             />
             <meta name="copyright" content="Nalbert Cerqueira 2023" />
             <meta property="og:image" content="https://nalbertdev.com/imgs/preview.webp" />
-            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:type" content="image/webp" />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="620" />
             <meta property="og:locale" content="pt_BR" />
